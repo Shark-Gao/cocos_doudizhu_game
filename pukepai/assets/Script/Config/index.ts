db05cff12efc26538eccd37291ab6b8d9f34275d
Shark-Gao <540167318@qq.com>
2026-06-05 00:23:01 +0800

    修复一些bug，连接正式服务器

--- pukepai/assets/Script/Config/index.ts
+++ pukepai/assets/Script/Config/index.ts
@@ -2,17 +2,21 @@
  * @author: sharkgao
  * @LastEditors: sharkgao
  */
+/*
+ * @author: sharkgao
+ * @LastEditors: sharkgao
+ */
 const dev = window.CC_DEBUG || true; //   && false
 
 export const CONFIG = {
     API_BASE_URL: dev
         ? 'http://localhost:3002'
-        : 'https://puke.liangziaha.online/api', // 打包线上接口
+        : 'https://doudizhu-game-server.onrender.com', // 打包线上接口
     RESOURCE_BASE_URL: dev
         ? 'http://localhost:3002/static'
-        : 'https://puke.liangziaha.online/static', // 打包线上静态资源
+        : 'https://doudizhu-game-server.onrender.com/static', // 打包线上静态资源
     // /ws 在nginx代理判断用的，/ws后面的随便
     SOCKET_BASE_URL: dev
         ? 'ws://localhost:3002/ws'
-        : 'wss://puke.liangziaha.online/ws', // 打包线上websocket接口
+        : 'wss://doudizhu-game-server.onrender.com/ws', // 打包线上websocket接口
 };
